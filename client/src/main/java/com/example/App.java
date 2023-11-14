067edef6efbbbe6cff96366dc6a00cea3937cb87
lai2-24 <lai2-24@itismeucci.com>
2023-11-14 14:16:36 +0100

    completato(culo)

--- client/src/main/java/com/example/App.java
+++ client/src/main/java/com/example/App.java
@@ -10,8 +10,11 @@ public class App {
     public static void main(String[] args) {
         Socket s = null;
         try {
-            s = new Socket("localhost", 3000);
+
             Scanner scan = new Scanner(System.in);
+            System.out.println("Inserisci l'indirizzo a cui ti vuoi connettere");
+            String indirizzo = scan.nextLine();
+            s = new Socket(indirizzo, 3000);
 
             BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
             DataOutputStream out = new DataOutputStream(s.getOutputStream());
@@ -19,14 +22,19 @@ public class App {
             System.out.println("Inserici il tuo nickname:");
             out.writeBytes(scan.nextLine() + "\n");
 
+            String messaggio = "";
             //starto il tubo di ascolto continuo
             Ascolto a = new Ascolto(s);
             a.start();
+
             do {
                 //scrivo 
-                out.writeBytes(scan.nextLine()+ "\n");
+                messaggio = scan.nextLine();
+                out.writeBytes(messaggio + "\n");
+            } while (!messaggio.equals("/q") && !messaggio.equals("/Q"));
 
-            } while (true);
+            s.close();
+            System.out.println("COMUNICAZIONE TERMINATA");
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
--- server/src/main/java/com/example/Frate.java
+++ server/src/main/java/com/example/Frate.java
@@ -18,6 +18,7 @@ public class Frate extends Thread {
         try {
             this.out = new DataOutputStream(s.getOutputStream());
         } catch (Exception e) {
+            System.out.println("Errore nel creare il Thread Frate");
         }
     }
 
@@ -29,6 +30,56 @@ public class Frate extends Thread {
         return this.s;
     }
 
+    // Manda un messaggio a tutti
+    public void broadcast(String username, String messaggio) {
+        try {
+            for (int i = 0; i < users.size(); i++) {
+                if (this.s.equals(users.get(i).getSocket()))
+                    continue;
+                Frate nuovo = users.get(i);
+                nuovo.getFOutputStream().writeBytes(username + ": " + messaggio + "\n");
+            }
+        } catch (Exception e) {
+            System.out.println("Problema nel Broadcast");
+        }
+    }
+
+    // Trova il posto nell'array di un utente
+    public int trovaUser(String name) {
+        for (int i = 0; i < users.size(); i++) {
+            String due = "/" + users.get(i).getName();
+            if (due.equals(name)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // controllo se username è già presente
+    public String controlloUser(String username, BufferedReader in) {
+
+        try {
+            boolean controllo = false;
+            do {
+                controllo = false;
+                for (int i = 0; i < users.size(); i++) {
+                    if (users.get(i).getName().equals(username)) {
+                        controllo = true;
+                    }
+                }
+                if (controllo == true) {
+                    out.writeBytes("Username gia' in utilizzo\n");
+                    username = in.readLine();
+                }
+            } while (controllo == true);
+
+        } catch (Exception e) {
+            System.out.println("Errore nel controllo di doppio user");
+        }
+        System.out.println("NUOVO UTENTE - " + username);
+        return username;
+    }
+
     public void run() {
 
         System.out.println("in client si è connesso");
@@ -36,28 +87,57 @@ public class Frate extends Thread {
         try {
             BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
 
-            String username = in.readLine();
-            System.out.println("NUOVO UTENTE - " + username);
-            //metti nome al thread
+            String username = controlloUser(in.readLine(), in);
+            // metti nome al thread
             Thread.currentThread().setName(username);
 
             String messaggio = new String();
 
             while (true) {
-                messaggio = in.readLine();
+                // se il client è solo non permette invio di messaggi
+                if (users.size() < 1) {
+                    continue;
+                } else {
+                    messaggio = in.readLine();
+                    // controllo se l'utente vuole inviare messaggio privato
+                    int num = trovaUser(messaggio);
 
-                System.out.println(messaggio);
-                System.out.println(users.size());
-                for (int i = 0; i < users.size(); i++) {
-                    if (this.s.equals(users.get(i).getSocket()))
-                        continue;
-                    Frate nuovo = users.get(i);
-                    nuovo.getFOutputStream().writeBytes(username + ": " + messaggio + "\n");
+                    // l'utente vuole mandare in privato
+                    if (num != -1) {
+
+                        messaggio = in.readLine();
+                        try {
+                            Frate nuovo = users.get(num);
+                            // invio in privato
+                            nuovo.getFOutputStream().writeBytes(username + " (messaggio privato): " + messaggio + "\n");
+
+                        } catch (Exception e) {
+                            System.out.println("Errore nel messaggio privato");
+                        }
+
+                        // mando in broadcast
+                    } else {
+
+                        // broadcast a tutti
+                        if (!messaggio.equals("/q") && !messaggio.equals("/Q")) {
+
+                            System.out.println(username + ": " + messaggio);
+                            broadcast(username, messaggio);
+
+                            // uscita
+                        } else {
+                            s.close();
+                            broadcast("SERVER",
+                                    "L'utente " + Thread.currentThread().getName() + " e' uscito dalla conversazione");
+                            break;
+                        }
+
+                    }
                 }
             }
 
         } catch (Exception e) {
-            System.out.println("problem");
+            System.out.println("problema");
         }
     }
 }
